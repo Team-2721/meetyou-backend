42b4672cf6c667fc6948508f144062c1d9bc7316
hans97a <byby8992@naver.com>
2023-09-21 10:55:07 +0900

    chore: user model avatar field

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.shortcuts import reverse
+from core.utils import upload_to
 
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, verbose_name="닉네임")
+    avatar = models.ImageField(
+        upload_to=upload_to("avatars", True), blank=True, verbose_name="프로필 사진"
+    )
