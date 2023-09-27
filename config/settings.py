5ecd681f97d18b51145038327bca483388eb70d8
hans97a <byby8992@naver.com>
2023-09-27 16:03:04 +0900

    chore: allowed host for aws

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 
 # Application definition
