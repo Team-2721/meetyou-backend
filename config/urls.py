7529f760ceef635efcdabf57515a499c8a3b1429
hans97a <byby8992@naver.com>
2023-10-17 17:19:37 +0900

    chore: remove drf yasg

--- config/urls.py
+++ config/urls.py
@@ -20,36 +20,9 @@ from django.urls import path, re_path
 from django.urls.conf import include
 from django.conf.urls.static import static
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="MeetU",
-        default_version="0.1",
-        description="MeetU Backend API DOCS",
-        contact=openapi.Contact(email="byby8992@naver.com"),  # 부가정보
-        license=openapi.License(name="MIT"),  # 부가정보
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 
 urlpatterns = [
-    re_path(
-        r"swagger(?P<format>\.json|\.yaml)",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        r"swagger",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    # path(
-    #     r"redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc-v1"
-    # ),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
 ]
