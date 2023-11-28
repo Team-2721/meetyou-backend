ac91be3d513eecdc32fcc2792cfd65cccf860515
hans97a <byby8992@naver.com>
2023-11-28 16:52:19 +0900

    test: web socket api

--- room/consumer.py
+++ room/consumer.py
@@ -23,107 +23,112 @@ def get_user(username):
 
 class RoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_pk = self.scope["url_route"]["kwargs"]["room_pk"]
-        self.room_group_name = f"chat_{self.room_pk}"
-        user = self.scope["user"]
+        return await super().connect()
 
-        try:
-            with transaction.atomic():
-                self.room = models.Room.objects.get(pk=self.room_pk)
-        except Exception as e:
-            return
 
-        if user.is_authenticated:
-            self.attendee, _ = models.Attendee.objects.get_or_create(
-                user=user, room=self.room
-            )
-            # 아래 코드에서 그룹 추가하는 건 해당 사람이 알림 켰는지 확인하고 나서 group_add 실행
-            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-            await self.accept()
-        else:
-            return  # 없는 경우 소켓 연결이 되지 않은 상태에서 메시지 전달하는 법
+# class RoomConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.room_pk = self.scope["url_route"]["kwargs"]["room_pk"]
+#         self.room_group_name = f"chat_{self.room_pk}"
+#         user = self.scope["user"]
 
-        # if user.is_authenticated:
-        #     self.attendee, _ = models.Attendee.objects.get_or_create(
-        #         user=user, room=room
-        #     )
+#         try:
+#             with transaction.atomic():
+#                 self.room = models.Room.objects.get(pk=self.room_pk)
+#         except Exception as e:
+#             return
 
-        #     await login(self.scope, user)  # 아래 코드에서 그룹 추가하는 건 해당 사람이 알림 켰는지 확인하고 나서
-        #     await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        #     await self.accept()
-        # else:
-        #     return  # 없는 경우 소켓 연결이 되지 않은 상태에서 메시지 전달하는 법
+#         if user.is_authenticated:
+#             self.attendee, _ = models.Attendee.objects.get_or_create(
+#                 user=user, room=self.room
+#             )
+#             # 아래 코드에서 그룹 추가하는 건 해당 사람이 알림 켰는지 확인하고 나서 group_add 실행
+#             await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+#             await self.accept()
+#         else:
+#             return  # 없는 경우 소켓 연결이 되지 않은 상태에서 메시지 전달하는 법
 
-    async def disconnect(self, code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+#         # if user.is_authenticated:
+#         #     self.attendee, _ = models.Attendee.objects.get_or_create(
+#         #         user=user, room=room
+#         #     )
 
-    async def receive(self, text_data):
-        text = json.loads(text_data)
-        send_type = text["send_type"]
-        user = self.scope["user"]
+#         #     await login(self.scope, user)  # 아래 코드에서 그룹 추가하는 건 해당 사람이 알림 켰는지 확인하고 나서
+#         #     await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+#         #     await self.accept()
+#         # else:
+#         #     return  # 없는 경우 소켓 연결이 되지 않은 상태에서 메시지 전달하는 법
 
-        if send_type == "done":
-            try:
-                with transaction.atomic():
-                    room = self.room
+#     async def disconnect(self, code):
+#         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-                    votes = models.Vote.objects.filter(attendee=self.attendee)
-                    if votes:
-                        votes.delete()
+#     async def receive(self, text_data):
+#         text = json.loads(text_data)
+#         send_type = text["send_type"]
+#         user = self.scope["user"]
 
-                    dates = text["dates"]
+#         if send_type == "done":
+#             try:
+#                 with transaction.atomic():
+#                     room = self.room
 
-                    vote_dates = [
-                        models.Vote(attendee=self.attendee, date=date) for date in dates
-                    ]
-                    models.Vote.objects.bulk_create(vote_dates)
+#                     votes = models.Vote.objects.filter(attendee=self.attendee)
+#                     if votes:
+#                         votes.delete()
 
-                    self.attendee.is_completed = True
-                    self.attendee.save()
+#                     dates = text["dates"]
 
-                    attendees_number = models.Attendee.objects.filter(room=room).count()
+#                     vote_dates = [
+#                         models.Vote(attendee=self.attendee, date=date) for date in dates
+#                     ]
+#                     models.Vote.objects.bulk_create(vote_dates)
 
-                    if attendees_number == room.attendee_number:  # 알림 전 체크 후 전송
-                        Notification.objects.create(
-                            user=user,
-                            room=room,
-                            message=f"{room.name}의 모든 투표가 완료되었습니다. 결과를 확인해 주세요!",
-                        )
-                        await self.channel_layer.group_send(
-                            self.room_group_name,
-                            {
-                                "ok": True,
-                                "message": "모든 투표가 완료되었습니다. 결과를 확인해 주세요!",
-                                "room": room.pk,
-                                "type": "chat.message",
-                            },
-                        )
-                        await self.channel_layer.group_discard(
-                            self.room_group_name, self.channel_name
-                        )
-                    else:
-                        await self.send(
-                            text_data=json.dumps(
-                                {
-                                    "message": "투표 완료되었습니다. 다른 참가자들이 모두 투표 하면 알려드릴게요!",
-                                    "ok": True,
-                                }
-                            )
-                        )
+#                     self.attendee.is_completed = True
+#                     self.attendee.save()
 
-            except Exception as e:
-                print("웹소켓 에러 ! -> ", e)
-                # self.send(text_data=json.dumps({"message": e}))
+#                     attendees_number = models.Attendee.objects.filter(room=room).count()
 
-    async def chat_message(self, event):
-        await self.send(
-            text_data=json.dumps({"message": event["message"], "room": event["room"]})
-        )
+#                     if attendees_number == room.attendee_number:  # 알림 전 체크 후 전송
+#                         Notification.objects.create(
+#                             user=user,
+#                             room=room,
+#                             message=f"{room.name}의 모든 투표가 완료되었습니다. 결과를 확인해 주세요!",
+#                         )
+#                         await self.channel_layer.group_send(
+#                             self.room_group_name,
+#                             {
+#                                 "ok": True,
+#                                 "message": "모든 투표가 완료되었습니다. 결과를 확인해 주세요!",
+#                                 "room": room.pk,
+#                                 "type": "chat.message",
+#                             },
+#                         )
+#                         await self.channel_layer.group_discard(
+#                             self.room_group_name, self.channel_name
+#                         )
+#                     else:
+#                         await self.send(
+#                             text_data=json.dumps(
+#                                 {
+#                                     "message": "투표 완료되었습니다. 다른 참가자들이 모두 투표 하면 알려드릴게요!",
+#                                     "ok": True,
+#                                 }
+#                             )
+#                         )
 
-    async def invalid_room(self, event):
-        await self.send(
-            text_data=json.dumps({"message": event["message"], "ok": event["ok"]})
-        )
+#             except Exception as e:
+#                 print("웹소켓 에러 ! -> ", e)
+#                 # self.send(text_data=json.dumps({"message": e}))
+
+#     async def chat_message(self, event):
+#         await self.send(
+#             text_data=json.dumps({"message": event["message"], "room": event["room"]})
+#         )
+
+#     async def invalid_room(self, event):
+#         await self.send(
+#             text_data=json.dumps({"message": event["message"], "ok": event["ok"]})
+#         )
 
 
 # class RoomConsumer(AsyncWebsocketConsumer):
